4e947ad9cc3055fb29e8ca323b599149c00bf048
SilentDepth <silentdepth@outlook.com>
2022-11-23 23:31:59 +0800

    feat(web): basic text color support

--- packages/web/src/app.vue
+++ packages/web/src/app.vue
@@ -4,6 +4,7 @@ import Tester from './tester.vue'
 
 let sticker = $ref(null as typeof TheSticker | null)
 let text = $ref('你好世界')
+let color = $ref('#000')
 
 function download (format: string) {
   const a = document.createElement('a')
@@ -20,11 +21,14 @@ div(class="min-h-screen py-10 bg-black flex flex-col items-center space-y-5")
     img(src="/icon.png" class="mx-auto" style="width: 64px; height: 64px;")
     h1(class="mt-2 font-bold text-center" style="font-family: 'Noto Serif SC';") Notion 贴纸生成器
 
-  p(class="text-neutral-400") 1. 输入内容文本（最多 4 个字符）
-  input(v-model="text" type="text" class="box-content w-[4em] px-[1em] py-[0.25em] text-lg bg-neutral-800 text-white border border-neutral-600 rounded outline-none text-center focus:border-neutral-500")
+  p(class="text-neutral-400") 1. 输入文本并选择颜色（最多 4 个字符）
+  div(class="flex gap-2")
+    input(v-model="text" type="text" class="flex-none box-content w-[4em] px-[1em] py-[0.25em] text-lg bg-neutral-800 text-white border border-neutral-600 rounded outline-none text-center focus:border-neutral-500")
+    div(class="flex-none w-10 border border-neutral-600 rounded cursor-pointer relative" :style="{ backgroundColor: color }")
+      input(v-model="color" type="color" class="absolute inset-0 w-full h-full opacity-0")
 
   p(class="text-neutral-400") 2. 预览生成效果（有 Bug 属正常）
-  TheSticker(ref="sticker" :text="text")
+  TheSticker(ref="sticker" :text="text" :color="color")
 
   p(class="text-neutral-400") 3. 点击下载按钮（iOS 不支持生成为 WebP）
   div(class="flex flex-col space-y-5")
