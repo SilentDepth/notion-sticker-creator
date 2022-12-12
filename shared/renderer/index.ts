f341453a728d58cd716dfb962de8c54dd1c6a1c7
SilentDepth <silentdepth@outlook.com>
2022-12-12 18:31:23 +0800

    feat(renderer): experimental support of emoji (Noto Color Emoji)

--- shared/renderer/index.ts
+++ shared/renderer/index.ts
@@ -38,6 +38,12 @@ export default async function render<T = unknown> (input: T, arg1?: boolean | Pa
       fonts: [
         { name: 'Noto Serif SC', weight: 400, style: 'normal', data: font },
       ],
+      async loadAdditionalAsset (code: string, segment: string) {
+        if (code === 'emoji') {
+          const codePoint = [...segment].map(s => s.codePointAt(0)!).filter(c => c < 0xFE00 || 0xFE0F < c).map(c => c.toString(16).padStart(4, '0')).join('_')
+          return `https://raw.githubusercontent.com/googlefonts/noto-emoji/main/svg/emoji_u${codePoint}.svg`
+        }
+      },
     },
   )
 }
